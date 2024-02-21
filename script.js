5ddad70939d488b50379214858d53519d6767d92
PukinCAT <128095540+ApukinDEV@users.noreply.github.com>
2024-02-21 14:51:31 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var z=0;
 bounds = [];
 pacmanpos = [];
+wallshor = [];
 key = 0;
 let y=0,x=0;
 function start(){
@@ -9,36 +10,79 @@ function start(){
     bounds[1] = box.offsetLeft;
     bounds[2] = box.offsetHeight;
     bounds[3] = box.offsetWidth;
-}
-function keypress(){
-    if(event.key=="ArrowRight" && pacmanpos[1]+pacmanpos[3]<bounds[3]-10){
-        key=event.key
-    }else if(event.key=="ArrowLeft" && pacmanpos[1]>0){
-        key=event.key
-    }else if(event.key=="ArrowUp" && pacmanpos[0]>0){
-        key=event.key
-    }else if(event.key=="ArrowDown" && pacmanpos[0]+pacmanpos[2]<bounds[2]-10){
-        key=event.key
+    for(i=0;i<1;i++){
+        wall=document.getElementById('obstaclehor'+(i+1));
+        wallshor[i*4] = wall.offsetTop;
+        wallshor[(i*4)+1] = wall.offsetLeft;
+        wallshor[(i*4)+2] = wall.offsetHeight+5;
+        wallshor[(i*4)+3] = wall.offsetWidth;
     }
-    console.log(key);
+    MyInterval = setInterval(move, 1)
 }
-setInterval(move, 5)
-function move(){
+function inbounds(direction){
     pacman = document.getElementById('pacman');
     pacmanpos[0] = pacman.offsetTop;
     pacmanpos[1] = pacman.offsetLeft;
     pacmanpos[2] = pacman.offsetHeight;
     pacmanpos[3] = pacman.offsetWidth;
-    console.log(bounds)
-    if(key=="ArrowRight" && pacmanpos[1]+pacmanpos[3]<bounds[3]-10){
+    if(direction == "ArrowRight"){
+        return(pacmanpos[1]+pacmanpos[3]<bounds[3]-10);
+    }else if(direction == "ArrowLeft"){
+        return(pacmanpos[1]>0);
+    }else if(direction == "ArrowUp"){
+        return(pacmanpos[0]>0);
+    }else if(direction == "ArrowDown"){
+        return(pacmanpos[0]+pacmanpos[2]<=bounds[2]-10);
+    }else{
+        return false;
+    }
+}
+function nearwall(direction){
+    pacman = document.getElementById('pacman');
+    pacmanpos[0] = pacman.offsetTop;
+    pacmanpos[1] = pacman.offsetLeft;
+    pacmanpos[2] = pacman.offsetHeight;
+    pacmanpos[3] = pacman.offsetWidth;
+    if(
+        ((pacmanpos[1]+pacmanpos[3]<wallshor[1] && pacmanpos[1]<wallshor[1]) ||  (pacmanpos[1]>wallshor[3]+wallshor[1] && pacmanpos[1]+pacmanpos[3]>wallshor[3]+wallshor[1]))&&((pacmanpos[0]>wallshor[0] && pacmanpos[0]+pacmanpos[3]>wallshor[0]) || (pacmanpos[0]<wallshor[0]+wallshor[2] && pacmanpos[0]+pacmanpos[3]<wallshor[0]+wallshor[2]))
+    )
+    {
+        console.log("jestes w scianie");
+        return(pacmanpos[1]);
+    }else if(direction == "ArrowLeft"){
+        return(pacmanpos[1]>0);
+    }else if(direction == "ArrowUp"){
+        return(pacmanpos[0]>0);
+    }else if(direction == "ArrowDown"){
+        return(pacmanpos[0]+pacmanpos[2]<=bounds[2]-10);
+    }else{
+        return false;
+    }
+}
+function blocked(){
+    if(key=="ArrowRight" && inbounds(key)){
+        pacman.style="transform:rotate(0deg)"
         x++;
-    }else if(key=="ArrowLeft" && pacmanpos[1]>0){
+    }else if(key=="ArrowLeft" && inbounds(key)){
+        pacman.style="transform:rotate(180deg)"
         x--;
-    }else if(key=="ArrowUp" && pacmanpos[0]>0){
+    }else if(key=="ArrowUp" && inbounds(key)){
+        pacman.style="transform:rotate(270deg)"
         y--;
-    }else if(key=="ArrowDown" && pacmanpos[0]+pacmanpos[2]<bounds[2]-10){
+    }else if(key=="ArrowDown" && inbounds(key)){
+        pacman.style="transform:rotate(90deg)"
         y++;
     }
+}
+function keypress(){
+    if(inbounds(event.key)){
+        key=event.key
+    }
+}
+function move(){
+    nearwall(key);
+    pacman = document.getElementById('pacman');
+    blocked();
     pacman.style.top = y+'px';
     pacman.style.left = x+'px';
 }
